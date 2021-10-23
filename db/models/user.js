21c0afa5860be4fec9f26506894bbe0c34efb37f
stephaniev <stephanieemelina13@gmail.com>
2021-10-23 00:45:48 -0400

    up to date with main

--- db/models/user.js
+++ db/models/user.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     Users.init({
         email: DataTypes.STRING,
         username: {type: DataTypes.STRING(30), allowNull: false, unique: true},
-        password: {type: DataTypes.STRING, allowNull: false, unique: true}
+        password: {type: DataTypes.STRING, allowNull: false}
     }, {
         sequelize,
         modelName: 'User',
